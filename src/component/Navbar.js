c465f6b45bd7a456501457148840224f38292183
Shudhanshu Singh <shudhanshusingh051@gmail.com>
2023-02-17 15:30:47 +0530

    some routing made

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
     <div className="w-full flex justify-around   align-middle p-2">
       <div className="flex-none flex justify-center align-middle">
-        <h2 className="text-3xl font-bold p-4 hover:cursor-pointer text-blue-800 ">TrueCertifyer</h2>
+        {location.pathname !== "/" ? (
+          <h2 onClick={()=>navigate(-1)} className="text-3xl font-bold p-4 hover:cursor-pointer text-blue-800 ">
+            ←
+          </h2>
+        ) : (
+          <h2 className="text-3xl font-bold p-4 hover:cursor-pointer text-blue-800 ">
+            TrueCertifyer
+          </h2>
+        )}
 
         <div className="flex-auto flex-row">
           {/* <ul className="flex flex-1 justify-evenly">
@@ -16,9 +27,15 @@ function Navbar() {
       </div>
 
       <div>
-        <button className="flex-1 border-blue-600 border-2 rounded-md p-2 pl-4 pr-4 mt-2  hover:bg-blue-600 hover:text-white">
-          Login / Register
-        </button>
+        {location.pathname === "/" ? (
+          <button onClick={()=>navigate('/login')} className="flex-1 border-blue-600 border-2 rounded-md p-2 pl-4 pr-4 mt-2  hover:bg-blue-600 hover:text-white">
+            Login / Register
+          </button>
+        ) : (
+          <h2 className="text-3xl font-bold p-4 hover:cursor-pointer text-blue-800 ">
+            TrueCertifyer
+          </h2>
+        )}
       </div>
     </div>
   );
